da0aa81d8a5d40ccf921f75f1f061ab8ff745030
Colin Li <ctli@usc.edu>
2025-03-30 11:34:32 -0700

    Add setup scripts

--- profile.py
+++ profile.py
@@ -23,10 +23,10 @@ node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-ST
 node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-STD";
 
 # Set up node 1 with Rust
-node1.addService(rspec.Execute(shell="bash", command="sudo apt-get update; sudo apt-get install -y rustc cargo"))
+node1.addService(rspec.Execute(shell="bash", command="/local/repository/setup_scripts/rust_setup.sh"))
 
 # Set up node 2 with k6
-node2.addService(rspec.Execute(shell="bash", command="sudo gpg -k; sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69; echo \"deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main\" | sudo tee /etc/apt/sources.list.d/k6.list; sudo apt-get update; sudo apt-get install -y k6"))
+node2.addService(rspec.Execute(shell="bash", command="/local/repository/setup_scripts/k6_setup.sh"))
 
 # Create a link between them
 link1 = request.Link(members = [node1, node2])
---
+++ setup_scripts/rust_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=1.85.1 -y
